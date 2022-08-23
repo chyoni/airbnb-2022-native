a53147b8931b2ae16ca9bc7eac0c08efe0dcb57d
최치원 <cw.choiit@choechiwon-ui-MacBookPro.local>
2022-08-23 22:52:15 +0900

    #02 SplashScreen

--- App.js
+++ App.js
@@ -1,16 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import AppLoading from 'expo-app-loading';
+import { useCallback, useEffect, useState } from 'react';
+import { Text, View } from 'react-native';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+
+//! Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
-  return <AppLoading />;
-}
+  const [appReady, setAppReady] = useState(false);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await Font.loadAsync(FontAwesome.font);
+
+        //! Artificially delay for two seconds to simulate a slow loading
+        await new Promise((resolve) => setTimeout(resolve, 2000));
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        //! Tell the application to render
+        setAppReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appReady) {
+      // ! This tells the splash screen to hide immediately! If we call this after
+      // ! `setappReady`, then we may see a blank screen while the app is
+      // ! loading its initial state and rendering its first pixels. So instead,
+      // ! we hide the splash screen once we know the root view has already
+      // ! performed layout.
+      await SplashScreen.hideAsync();
+    }
+  }, [appReady]);
+
+  if (!appReady) {
+    return null;
+  }
+  return (
+    <View
+      style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}
+      onLayout={onLayoutRootView}
+    >
+      <Text>SplashScreen Demo! 👋</Text>
+      <FontAwesome name={'newspaper-o'} />
+    </View>
+  );
+}
--- README.md
+++ README.md
@@ -7,3 +7,18 @@
 
   npm start
   ```
+
+- #02 SplashScreen
+
+  - AppLoading이 deprecated 되었다고 알려주더라. 이걸 쓰라네 요즘은
+
+  #### Cycle은 이런식이다.
+
+  1. SplashScreen.preventAutoHideAsync();
+  2. return null;
+  3. useEffect
+  4. setAppIsReady to true
+  5. rerendering because state changed
+  6. return (<View>...</View>)
+  7. useCallback (View에서 onLayout을 useCallback을 사용해서)
+  8. SplashScreen hide
