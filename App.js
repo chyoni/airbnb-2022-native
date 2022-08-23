9c8cd11df821ac429da4d4fa895ac72bd671a2aa
최치원 <cw.choiit@choechiwon-ui-MacBookPro.local>
2022-08-23 22:32:20 +0900

    #01 Init

--- App.js
+++ App.js
@@ -1,13 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import AppLoading from 'expo-app-loading';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return <AppLoading />;
 }
 
 const styles = StyleSheet.create({
---
+++ README.md
@@ -0,0 +1,9 @@
+# Airbnb 2022 clone app with React native + Redux + Expo
+
+- #01 Init
+
+  ```bash
+  npx create-expo-app my-app
+
+  npm start
+  ```
